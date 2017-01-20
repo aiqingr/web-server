f63cc4911d09fb8f30a83ebe2929ce3bd688f1c4
Ni Tongyang <ntyaiqingr@gmail.com>
2017-01-20 10:52:16 -0500

    all files transfer to the new repo

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3010;
+
+var middleware = require('./middleware.js');
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+//
+//about
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+  res.send('About us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+  console.log('Express server ' + PORT + ' started!');
+});
